ea3e7088ecaef5a35d3b46082e1047316b904326
hendri <hendri@go-jek.com>
2020-04-10 07:54:42 +0700

    update meta

--- application/views/backend/vw_home.php
+++ application/views/backend/vw_home.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <title>Covid19 | Pujotirto</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="CMS | Covid19 | DesaPujotirto" name="description" />
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
